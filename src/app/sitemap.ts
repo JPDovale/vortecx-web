ac5f2f47d77fc8af82539b6adc3bd86b432f4f65
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-07-31 20:59:05 -0300

    fix: solve site map

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,10 @@
+import { getPostsRequest } from "@/services/mainapi/posts/getPostsRequest";
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const response = await getPostsRequest();
+	const posts = response.data?.posts ?? [];
+
 	return [
 		{
 			url: "https://vortecxx.com",
@@ -12,7 +16,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			url: "https://vortecxx.com/articles",
 			lastModified: new Date(),
 			changeFrequency: "weekly",
-			priority: 0.2,
+			priority: 0.7,
 		},
+		...posts.map((post) => ({
+			url: `https://vortecxx.com/articles/${post.slug}`,
+			lastModified: new Date(),
+			changeFrequency: "yearly" as const,
+			priority: 0.4,
+		})),
 	];
 }
